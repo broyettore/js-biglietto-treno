8ffd6c7b2b64b269a24f320a652e3bed7abe7dbf
broyettore <broyettore@gmail.com>
2023-02-21 16:45:07 +0100

    Simple interface done, project completed

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,8 @@
 // VARIABILI 
 
-const utenteKm = Number(prompt("Quanti km vuole percorrere ?"))
-const utenteEtà = Number(prompt("Quanti anni ha ?"))
+const utenteNome = prompt("Ciao, come ti chiami ?");
+const utenteKm = Number(prompt("Quanti km vuole percorrere ?"));
+const utenteEtà = Number(prompt("Quanti anni ha ?"));
 const prezzoBase = 0.21 * utenteKm
 const scontoMinorenni = prezzoBase - (prezzoBase / 100) * 20
 const scontoAnziani = prezzoBase - (prezzoBase / 100) * 40
@@ -9,10 +10,17 @@ const scontoAnziani = prezzoBase - (prezzoBase / 100) * 40
 
 // CONDIZIONI DI SCONTO 
 
+document.querySelector(".user").innerHTML = `Ciao ! ${utenteNome}`
+document.querySelector(".distance").innerHTML = `Hai scelto di percorrere ${utenteKm} km`
+
 if (utenteEtà < 18) {
-    console.log(Number(scontoMinorenni.toFixed(2)))
+    document.querySelector(".price").innerHTML = `Il prezzo per questa distanza è di ${Number(scontoMinorenni.toFixed(2))}€`;
 } else if (utenteEtà > 65) {
-    console.log(Number(scontoAnziani.toFixed(2)))
+    document.querySelector(".price").innerHTML = `Il prezzo per questa distanza è di ${Number(scontoAnziani.toFixed(2))}€`;
 } else {
-    console.log(Number(prezzoBase.toFixed(2)))
+    document.querySelector(".price").innerHTML = `Il prezzo per questa distanza è di ${Number(prezzoBase.toFixed(2))}€`;
 }
+
+// RISULTATO
+
+document.querySelector(".summary").innerHTML = `Pronto per questa nuova avventura?`
